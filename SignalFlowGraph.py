2d9392a2109a5b1302983d831895bf5b81881521
ahmedyoussefg <a.goerany@gmail.com>
2024-04-12 17:29:13 +0200

    Implement SimplifyGraph() to simplify multi-edges into a single edge That single edge is the summation of all the multi-edges

--- SignalFlowGraph.py
+++ SignalFlowGraph.py
@@ -13,7 +13,22 @@ class SignalFlowGraph:
         
         self.visited = [False] * (self.number_of_nodes+1) #onebased
 
-    
+        # simplify multi edges
+        self.simplify_graph()
+
+    # Function to simplify multi edges (summation of gains for each edge)
+    def simplify_graph(self):
+        for node in self.graph:
+            edges_dict = {}  # To store summed gains for each destination vertex
+            for edge in self.graph[node]:
+                destination_vertex, gain = edge
+                if destination_vertex in edges_dict:
+                    edges_dict[destination_vertex] += gain
+                else:
+                    edges_dict[destination_vertex] = gain
+            # Update the adjacency list with simplified edges
+            self.graph[node] = [(dest, gain) for dest, gain in edges_dict.items()]
+
     def find_loops(self):
         for node in range(1,self.number_of_nodes+1):
             if not self.visited[node]:
@@ -21,6 +36,7 @@ class SignalFlowGraph:
                 self.dfs(node, node, [])
                 self.visited = [False] * (self.number_of_nodes+1) #onebased
         return self.loops
+    
     # node: current node, start: starting node of the cycle
     def dfs(self, node, start, path):
         self.visited[node]=True
@@ -43,7 +59,7 @@ class SignalFlowGraph:
                         break
 
                 if not skip:
-                    self.loops.append(path + [start])
+                    self.loops.append(path + [start] )
             elif not self.visited[child]:
                 self.dfs(child,start, path[:])
 
@@ -59,6 +75,14 @@ graph = {
     6: []  # No outgoing edges from 6
 }
 
+# Multi edges example
+# graph = {
+#     1: [(2, 3.5), (2, 7.5), (3, 5)], 
+#     2: [(3, 2)], 
+#     3: [(2, 1.5), (1, 1), (3, 0.5)],
+# }
+
 sfg = SignalFlowGraph(graph)
+print("Graph: ", sfg.graph)
 loops = sfg.find_loops()
 print("Individual Loops:", loops)
